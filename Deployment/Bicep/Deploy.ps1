89b593a0ce7ff3ee26b696010c9f31803bb8e5d6
rjjones79 <rjjones79@hotmail.com>
2026-06-15 14:47:07 +1000

    Updatedm deploy.ps1

--- Deployment/Bicep/Deploy.ps1
+++ Deployment/Bicep/Deploy.ps1
@@ -1,17 +1,18 @@
 param (
-	[string] $ResourceGroup
+	[string] $ResourceGroup,
+	[string] $ContainerRegistry,
+	[string] $Environment
 )
-
-
-az role assignment delete --assignee $clientId --scope $(az appconfig show -n 'apconfig-constructix-ae-dev-01' -g 'rg-constructix-dev-ae-01' --query id -o tsv)     
-az identity delete --name "electorcontainerappidentity01" --resource-group "$resourceGroup"
+#az role assignment delete --assignee $clientId --scope $(az appconfig show -n 'apconfig-constructix-ae-dev-01' -g 'rg-constructix-dev-ae-01' --query id -o tsv)     
+#az identity delete --name "electorcontainerappidentity01" --resource-group "$resourceGroup"
 
 $latestTag = az acr repository show-tags `
-  --name crelectordevae `
+  --name $ContainerRegistry `
   --repository electorservicesrepository `
   --orderby time_desc `
   --top 1 `
   --output tsv 
+
 Write-Host "Latest Tag:$latestTag" 
 # when put into the deployment pipeline will assign to 
 # Write-Host "##vso[task.setvariable variable=latestTag]$latestTag"
@@ -19,4 +20,4 @@ az deployment group create `
   --name AzureFunctionsOnContainerAppsDeploymentUserAssigned `
   --resource-group $ResourceGroup `
   --template-file main.bicep `
-  --parameters main-dev.bicepparam latestImageTag=$latestTag assignRoleToAppConfig=true
+  --parameters main-$Environment.bicepparam latestImageTag=$latestTag assignRoleToAppConfig=true
