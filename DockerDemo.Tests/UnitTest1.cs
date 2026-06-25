a6ac2cf56c5a1e2ba458a81c26d31cd3d3bc2bfa
rjjones79 <rjjones79@hotmail.com>
2026-06-25 21:38:42 +1000

    Updated Unit tests

--- DockerDemo.Tests/UnitTest1.cs
+++ DockerDemo.Tests/UnitTest1.cs
@@ -1,13 +1,20 @@
 ﻿namespace DockerDemo.Tests
 {
+    
     using dockerDemo.Customers.Services.Products;
+    using Microsoft.Extensions.Logging;
+    using Moq;
+
     public class ProductServiceTests
     {
         [Fact]
         public void Test1()
         {
-            var productService = new ProductService(new List<dockerDemo.Product>(), null);
-            Assert.True(productService == null);
+
+            var loggerMock = new Mock<ILogger<ProductService>>();
+
+            var productService = new ProductService(new List<dockerDemo.Product>(), loggerMock.Object);
+            Assert.True(productService != null);
         }
     }
 }
