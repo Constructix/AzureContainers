3d268be522928984e2188e31c3f9acb1b0a8b3f4
rjjones79 <rjjones79@hotmail.com>
2026-07-01 09:28:34 +1000

    Hold off

---
+++ webApi/tests/Ems.Api.Data.Respository.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Shouldly;
+
+namespace Ems.Api.Data.Respository.Tests;
+
+public class ConfirmUnitTestsRun
+{
+    [Theory]
+    [InlineData(9,1,10)]
+    public void ConfirmUnitTestsRunAsExpected(int first, int second, int expectedResult)
+    {
+        var sum = first + second;
+        expectedResult.ShouldBe(sum);        
+    }
+}
