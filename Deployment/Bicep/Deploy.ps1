3eacc5a6c2aae0ed560492057096c0444996b0cd
rjjones79 <rjjones79@hotmail.com>
2026-07-19 07:49:45 +1000

    Updated pipeline and added generating api key for APIM.

--- Deployment/Bicep/Deploy.ps1
+++ Deployment/Bicep/Deploy.ps1
@@ -4,6 +4,15 @@ param (
      [string] $DeploymentEnv, 
      [string] $RepositoryName 
 )
+$bytes = New-Object byte[] 32
+[System.Security.Cryptography.RandomNumberGenerator]::Create().GetBytes($bytes)
+$subscriptionKey = [Convert]::ToBase64String($bytes)
+
+Write-Host "Subscription Key value: '$subscriptionKey'"
+Write-Host "ContainerAppFqdn param value: '$ContainerAppFqdn'"
+Write-Host "Length: $($ContainerAppFqdn.Length)"
+
+
 
 Write-Host "ResourceGroup=$ResourceGroup ContainerRepository=$ContainerRepository DeploymentEnv=$DeploymentEnv RepositoryName=$RepositoryName"
 az deployment group create `
